c155d22055d4549cc25eb40939b67c67dff96e2d
ngchi19 <bnchi7015@gmail.com>
2024-05-18 20:06:11 +0700

    Create Cart screen and Signin screen

--- ChuyenDeWeb_CK/frontend/src/screens/ProductScreen.js
+++ ChuyenDeWeb_CK/frontend/src/screens/ProductScreen.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useReducer } from 'react';
 import { useContext, useEffect, useReducer } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
@@ -29,6 +29,7 @@ const reducer = (state, action) => {
 };
 
 function ProductScreen() {
+  const navigate = useNavigate();
   const params = useParams();
   const { slug } = params;
   const [{ loading, error, product }, dispatch] = useReducer(reducer, {
@@ -62,6 +63,8 @@ function ProductScreen() {
     ctxDispatch({
       type: 'CART_ADD_ITEM',
       payload: { ...product, quantity },
+    });
+    navigate('/cart');
   };
 
   return loading ? (
